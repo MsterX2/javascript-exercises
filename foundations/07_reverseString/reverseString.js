34e25dba557e3b283eee55e83a442733fc5a0399
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-05 18:22:46 +0200

    solverd excercises 6 and 7 from fundation excercises

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = ""
+    const iteration = string.length
+    for (let char of string) {
+        reversedString = char + reversedString;
+    }
+    return reversedString
 };
 
 // Do not edit below this line
