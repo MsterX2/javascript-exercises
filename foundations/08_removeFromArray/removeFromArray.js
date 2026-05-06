df8e0e962bf4208c50a73b4d602f1a1b108f91bb
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-06 21:03:31 +0200

    solved excercise 8 from foundation

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    let resultingArray = array.slice()
+    for (let number of numbers) {
+        let index = resultingArray.indexOf((number))
+        if (index !== -1) {
+            resultingArray = resultingArray.filter(
+                item => item !== number 
+            )
+        }
+    }
+    return resultingArray;
+
 };
 
 // Do not edit below this line
