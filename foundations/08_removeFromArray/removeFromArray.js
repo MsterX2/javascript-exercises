779371c776767edf1c91538ffb8e9f4db8b504e4
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-06 22:12:47 +0200

    clean trash from foundation excercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,12 +1,9 @@
 const removeFromArray = function(array, ...numbers) {
     let resultingArray = array.slice()
     for (let number of numbers) {
-        let index = resultingArray.indexOf((number))
-        if (index !== -1) {
             resultingArray = resultingArray.filter(
                 item => item !== number 
             )
-        }
     }
     return resultingArray;
 
